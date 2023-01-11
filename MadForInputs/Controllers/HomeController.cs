bbe0f0f3d4a2aaad8e6b7afb49a7677aee3d4362
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2023-01-11 14:28:25 -0700

    MadForInputs

---
+++ MadForInputs/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using MadForInputs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadForInputs.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult MadLibs(string Adjective, string Noun1, string Verb, string Adverb, string Noun2)
+		{
+			ViewBag.AD = Adjective;
+			ViewBag.N1 = Noun1;
+			ViewBag.VB = Verb;
+			ViewBag.AB = Adverb;
+			ViewBag.N2 = Noun2;
+
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
--- Movies/Controllers/HomeController.cs
+++ Movies/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace Movies.Controllers
 {
     public class HomeController : Controller
     {
+        private static int intCount = 0;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -23,7 +24,36 @@ namespace Movies.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Future()
+        {
+            return View();
+        }
+
+        public IActionResult Counter()
+        {
+            ViewBag.Count = intCount++;
+            return View();
+        }
+
+		public IActionResult Input()
+		{
+            ViewData["Title"] = "Input Form";
+			return View();
+		}
+
+		public IActionResult Output(string FirstName, string LastName)
+		{
+            ViewBag.FN = FirstName;
+            ViewBag.LN = LastName;
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
